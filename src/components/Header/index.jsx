4371f3a084403059c0f76ebc066bef7346a6fb78
choudurivenkatesh250-prog <choudurivenkatesh250@gmail.com>
2026-04-14 09:20:41 +0530

    new files added

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+const Header  = () => {
+
+    return(
+        <>
+        <header>
+            <div className='logo-container'>
+                <img src ="https://img.freepik.com/premium-vector/book-logo-design-concept_761413-7900.jpg?semt=ais_hybrid&w=740&q=80"/>
+           <p>Bookle</p>
+            </div>
+        </header>
+        </>
+    )
+}
+export default Header
